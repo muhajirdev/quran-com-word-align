eb74c3381143428e2666fb257c0a0094bc46fa1e
Muhammad Muhajir <muhajirframe@gmail.com>
2021-06-04 17:57:19 +0700

    fix: corner case implict ayah, surah ends, middle of the page

--- logic/keys.js
+++ logic/keys.js
@@ -1,4 +1,5 @@
 const { range, flatten, removeLastItem } = require('./utils')
+const totalAyah = require('../data/total-ayah-surah.json')
 
 const getKey = (currentLine) => {
   if (!currentLine) return null
@@ -10,20 +11,24 @@ const getKey = (currentLine) => {
 
 const normalLine = (lineItems) => lineItems.length === 3
 
-const generateKeys = (surah, lines) => {
+const generateKeys = (surah, lines, finishSurah) => {
   const ayahs = lines
     .filter(normalLine)
     .filter((line) => line[0] === surah)
     .map((line) => line[1])
 
   const minAyah = Math.min(...ayahs)
-  const maxAyah = Math.max(...ayahs)
+  const maxAyah = finishSurah ? totalAyah[surah] : Math.max(...ayahs)
 
   return range(maxAyah - minAyah + 1, minAyah).map((ayah) => `${surah}:${ayah}`)
 }
 
+const nextSurahIsInThisPage = (surah, surahs) => {
+  return surahs.includes(surah + 1)
+}
+
 // We're generating keys, instead of collecting keys from lines using `reduce` because some verse are hidden like `1:4`
-const getAllKeys = (lines) => {
+const getAllKeys = (lines, nextPageFirstKey) => {
   const normalLines = lines.filter(normalLine)
 
   const maxSurah = Math.max(...normalLines.map((line) => line[0]))
@@ -31,9 +36,11 @@ const getAllKeys = (lines) => {
 
   const surahs = range(maxSurah - minSurah + 1, minSurah)
 
-  const keys = surahs.map((surah) => generateKeys(surah, lines))
+  const keys = surahs.map((surah) =>
+    generateKeys(surah, lines, nextSurahIsInThisPage(surah, surahs))
+  )
 
-  // as noted in notes.txt, there's a virtual line at the end of each page. 
+  // as noted in notes.txt, there's a virtual line at the end of each page.
   // So, we remove that
   return removeLastItem(flatten(keys))
 }
--- solution.test.js
+++ solution.test.js
@@ -8,3 +8,41 @@ it('works', () => {
 
   expect(output).toEqual(expected)
 })
+
+it('handle the implicit ayah corner case, surah ends, middle of the page', () => {
+
+  
+  const input = [
+    [
+      [1],
+      [1, 1, 0],
+      [1, 2, 0],
+      [1, 3, 0],
+      [1, 5, 0],
+      [1, 6, 1],
+      [1, 6, 2],
+      [1, 6, 3],
+      [2],
+      [2, 1, 0],
+      [2, 2, 0],
+      [2, 3, 0],
+    ],
+  ]
+
+  const expected = [
+    {
+      '1:1': { page: 1, words: [1, 1, 1, 1, 1] },
+      '1:2': { page: 1, words: [2, 2, 2, 2, 2] },
+      '1:3': { page: 1, words: [3, 3, 3] },
+      '1:4': { page: 1, words: [3, 3, 3, 3] },
+      '1:5': { page: 1, words: [4, 4, 4, 4, 4] },
+      '1:6': { page: 1, words: [4, 5, 6, 7] },
+      '1:7': { page: 1, words: [7, 7, 7, 7, 7, 7, 7, 7, 7, 7] },
+      '2:1': { page: 1, words: [9, 9, 9, 9, 9, 9, 9, 9, 9, 9] },
+      '2:2': { page: 1, words: [10, 10, 10, 10, 10, 10, 10, 10, 10, 10] },
+    },
+  ]
+
+  const output = solution(input)
+  expect(output).toEqual(expected)
+})
